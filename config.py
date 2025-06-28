119f45e7249cd32bd8ae67663b5083c9801ca34e
aytee-80 <amogelangthompson@gmail.com>
2025-06-28 15:53:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 DB_HOST = "dpg-d0k6omre5dus73bgro6g-a.oregon-postgres.render.com"
 DB_USER = "aytee"
 DB_PASSWORD = "NtXZkPPiOqMpZ8roxYA40jhqQNy7MChZ"
-DB_NAME = "database_db_vwtj"
+DB_NAME = "Amo"
 DB_PORT = 5432
 SECRET_KEY = "Siya123"
