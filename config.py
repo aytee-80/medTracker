c78c9a25458e86bf0d0ff43995c38e23df73aeac
aytee-80 <amogelangthompson@gmail.com>
2025-06-28 16:22:17 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 DB_HOST = "dpg-d1fv227gi27c73e9s42g-a"
 DB_USER = "aytee"
 DB_PASSWORD = "NtXZkPPiOqMpZ8roxYA40jhqQNy7MChZ"
-DB_NAME = "Amo"
+DB_NAME = "database_db_vwtj"
 DB_PORT = 5432
 SECRET_KEY = "Siya123"
