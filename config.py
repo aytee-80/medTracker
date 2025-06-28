e2ae64ef2882b6ccc1f11abe1d083f286ffba2aa
aytee-80 <amogelangthompson@gmail.com>
2025-06-28 15:45:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 DB_HOST = "dpg-d0k6omre5dus73bgro6g-a.oregon-postgres.render.com"
-DB_USER = "aytee7"
-DB_PASSWORD = "LqpUqXEqCQ78jxDG7nc0Rk3qFHoDPOm0"
-DB_NAME = "medicatiion_app"
+DB_USER = "aytee"
+DB_PASSWORD = "NtXZkPPiOqMpZ8roxYA40jhqQNy7MChZ"
+DB_NAME = "database_db_vwtj"
 DB_PORT = 5432
 SECRET_KEY = "Siya123"
